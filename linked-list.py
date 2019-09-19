18c21b1f6524f02ea7f068896caa6e84dbd18734
LennyAtomz138 <54559335+LennyAtomz138@users.noreply.github.com>
2019-09-19 17:44:00 -0400

    Updated linked-list.py
    Created Node class and add initialization definition.

--- linked-list.py
+++ linked-list.py
@@ -1,1 +1,9 @@
 # 19 SEP 2019 -- Lenny Adams -- Linked List in Python
+
+# Begin by creating a Node class.
+class Node:
+  
+  # Initialize the Node class.
+  def __init__(self, data=None, next_node=None):
+    self.data = data
+    self.next_node = next_node
