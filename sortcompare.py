eebbe1581f3a85440d80a4a693ed7110e0a8c22a
Lenny Adams <54559335+LennyAtomz138@users.noreply.github.com>
2024-08-26 21:58:02 -0400

    Add files via upload

---
+++ sortcompare.py
@@ -0,0 +1,21 @@
+import sys
+import csv
+
+def parse(f):
+    """Takes in an open CSV file of the format first, last, email and turns
+    it into a list that we can work with
+    """
+    return [line.split(', ') for line in f]
+
+if __name__ == "__main__":
+	if len(sys.argv) < 2:
+		print('Please use: python sortcompare.py <file>')
+		sys.exit(-1)
+	file1 = sys.argv[1]
+
+	with open(file1, 'r') as f1:
+		list1 = parse(f1)[0][0].upper().strip('\n').split(',')
+		if '' in list1:
+			list1.remove('')
+		for i in sorted(list1):
+			print(i)
